d2ff620940fc5e1f21fb5a0463cb6ed095eae169
graalumj <alexgraalum@yahoo.com>
2024-04-03 15:04:20 -0700

    First Problem Complete

--- 01_Grades/Grades.cpp
+++ 01_Grades/Grades.cpp
@@ -1,7 +1,45 @@
+//Includes
 #include <iostream>
 
+int getUserInput();
+
+//Functions
 int main(int argc, char** argv) {
-     std::cout << "Hello World!\n";
+     int x = 0;
+     
+     std::cout << "Hello!\nPlease enter your grade [0-100]: ";
+
+     x = getUserInput();
+
+     if (x == 100) {
+          std::cout << "Congrats! You got a perfect score!\n";
+     }
 
      return 0;
+}
+
+////Get User Input
+//Use cin to get user input
+//Handle invalid user inputs
+//Return valid entry
+int getUserInput() {
+     int x = 0;
+
+     std::cin >> x;
+
+     while (std::cin.fail()) {
+          std::cout << "That was not an integer. Enter an integer from 0 to 100: ";
+          std::cin.clear();
+          std::cin.ignore(INT_MAX, '\n');
+          std::cin >> x;
+     }
+
+     if ((x < 0) || (x > 100)) {
+          std::cout << "That entry is not within the valid bounds. Please keep the entry from 0 to 100: ";
+          std::cin.clear();
+          std::cin.ignore(INT_MAX, '\n');
+          return getUserInput();
+     }
+
+     return x;
 }
--- 01_Grades/README.md
+++ 01_Grades/README.md
@@ -12,11 +12,11 @@ Requires:
 
 Write a program that allows the user to enter the grade scored in a class (0-100).
 
-If the user scored a 100, notify the user that they got a perfect score.
+[x] If the user scored a 100, notify the user that they got a perfect score.
 
-**X** Modify the program so that if the user scored a 90-100, it informs the user that they scored an A
+[ ] **X** Modify the program so that if the user scored a 90-100, it informs the user that they scored an A
 
-**X** Modify the program so that it will notify the user of their letter grade
+[ ] **X X** Modify the program so that it will notify the user of their letter grade
 
 | Score | Grade |
 |-------|-------|
